6d438bc5ae33172e205d5b46c17dddfb5d4e4bcb
healyjames <38892951+healyjames@users.noreply.github.com>
2018-07-26 12:58:39 +0100

    Update FileRename.java

--- FileRename.java
+++ FileRename.java
@@ -3,16 +3,26 @@ package rename;
 import java.io.*;
 import java.util.Scanner;
 
+/**
+ * This program renames files so they can be uploaded to www.thetravelvisacompany.co.uk 
+ * @author James
+ *
+ */
+
 public class FileRename {
 	
-	/**
-	 * FIX: Countries with two words in their name
-	 * 
-	 * IDEA 1: save files into another destination chosen by the user
-	 */
+
+	 //IDEA 1: save files into another destination chosen by the user
+	
+	//IDEA 2: be able to supply the Tourist Requirements directory. The program goes into each country file and updates the pdf properties and can be saved into a new file. Need to think about how to handle tourist visa and evisa directories within that country folder and if there are none.
 	
 	private static String[] type = {"Tourist", "Business", "Media", "Study", "eVisa"};
 	private static File dest;
+	
+	/**
+	 * This method takes the name of each file in the directory, finds the name of the country and replaces the rest of the file name with the correct extension
+	 * @param path
+	 */
 	
 	public static void getFileName(String path) {
 		
@@ -24,16 +34,18 @@ public class FileRename {
 		//Find the files inside directory
 		File[] file = root.listFiles();
 		
-		//Loop through each document in that file
+		//Loop through each document in that directory
 		for(int i = 0; i < file.length; i++) {
 			
-			//If the current file is a file
+			System.out.println(file[i].getName());
+			
+			//If the current file is a file and the name does not contain "_".
 			if(file[i].isFile() && !file[i].getName().contains("_")) {
 				
 				//Loop through each type of visa
 				for(String s : type) {
 					
-					//If that word contains a visa type
+					//If the file name contains a visa type
 					if(file[i].getName().contains(s)) {
 						
 						String country;
@@ -65,6 +77,11 @@ public class FileRename {
 		checkError(error);
 		
 	}
+	
+	/**
+	 * Method to check if there are any errors and print success/failure messages
+	 * @param error
+	 */
 	
 	public static void checkError(boolean error) {
 
@@ -99,5 +116,4 @@ public class FileRename {
 		getFileName(input);
 		
 	}
-
 }
